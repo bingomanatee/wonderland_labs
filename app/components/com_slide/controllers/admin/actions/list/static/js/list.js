3ca779582f8c92368cfb57b4d458988829e177e8
pc-dedelhart <david.edelhart@personalcapital.com>
2012-10-07 13:18:37 -0700

    update to slide admin

--- app/components/com_slide/controllers/admin/actions/list/static/js/list.js
+++ app/components/com_slide/controllers/admin/actions/list/static/js/list.js
@@ -11,9 +11,9 @@ angular.module('slidesServices', ['ngResource']).factory('Slideshows',
         });
     }).factory('Slides',
     function ($resource) {
-        return $resource('/admin/slides/:slideshow', {slideshow:"@_id"}, {
+        return $resource('/admin/slideshow/slides/:_id', {_id:"@_id"}, {
             get:{method:'GET'},
-            query:{method:'GET'}, // note - slideshows is in node "slideshows"
+            query:{method:'GET', isArray: true}, // note - slideshows is in node "slideshows"
             add:{method:'POST' },
             update:{method:'PUT' },
             delete:{method:'DELETE'}
@@ -32,17 +32,102 @@ function SlideCtrl($scope, $filter, $compile, Slideshows, Slides) {
 
     $scope.view = function(ss){
         $scope.current_slideshow = ss;
-      //  $scope.slides = Slides.query(ss)
+        $scope.slides = Slides.query(ss, function(slides){
+            console.log('slides: ', slides);
+        })
     }
 
     $scope.add_slide = function(){
+        new_slide_ck.setData('');
         $('#newSlide').modal({backdrop: true, show: true})
     }
+
+    $scope.edit_slide = function(slide){
+        $scope.current_slide = slide;
+        console.log('editing ', slide);
+        edit_slide_ck.setData(slide.content);
+        $('#editSlide').modal({backdrop: true, show: true})
+    }
+
+    $scope.ordered_slides = function(){
+        var out = _.sortBy($scope.slides, function(s){
+            return parseInt(s.weight);
+        })
+
+        return out;
+    }
+
+   function _slide_summary(){
+      return _.reduce($scope.slides, function(w, slide){
+           w[slide._id] = {weight: slide.weight, title: slide.title};
+           return w;
+       }, {});
+   }
+
+    function _reorder(summary){
+        var weights = _slide_summary();
+        console.log('reorder: summary', summary, weights);
+        if (weights){
+            $.post('/admin/slideshow/order_slides', {weights: weights});
+        }
+    }
+
+    $scope.$watch(function(){
+        return angular.toJson(_slide_summary());
+    }, _reorder);
 
     $scope.new_slide = {
         title: 'New Slide',
         notes: '',
         content: ''
+    }
+
+    $scope.save_new_slide = function(){
+        $scope.new_slide.slideshow = $scope.current_slideshow._id;
+        $scope.new_slide.content = new_slide_ck.getData();
+        console.log('saving new slide ', $scope.new_slide);
+        Slides.add($scope.new_slide, function(e, r){
+            console.log('added slide ', e, r);
+
+            $scope.slides = Slides.query($scope.current_slideshow, function(slides){
+                console.log('slides: ', slides);
+                $('#newSlide').modal('hide')
+            })
+        })
+
+        $scope.new_slide = {
+            title: 'New Slide',
+            notes: '',
+            content: ''
+        }
+    }
+
+    $scope.save_current_slide = function(){
+        var slide = $scope.current_slide;
+        slide.content = edit_slide_ck.getData();
+        console.log('edit - update slide ', slide);
+        Slides.update(slide, function(e, r){
+            console.log('updated slide ', e, r);
+
+            $scope.slides = Slides.query($scope.current_slideshow, function(slides){
+                console.log('slides: ', slides);
+                $('#editSlide').modal('hide')
+            })
+        })
+
+        $scope.new_slide = {
+            title: 'New Slide',
+            notes: '',
+            content: ''
+        }
+    }
+
+    $scope.new_slideshow = function(){
+        document.location="/admin/slideshow/new"
+    }
+
+    $scope.delete_slide = function(slide){
+        Slides.delete(slide)
     }
 }
 
